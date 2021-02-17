267554f954cd39ec108b2615106f32b7b87a906a
Erick Araujo <erick.faraujo@gmail.com>
2021-02-17 18:19:54 -0300

    Criando projeto de testes

---
+++ MetWorkingMatch.Test/Features/ListarPedidosEnviados.feature
@@ -0,0 +1,13 @@
+﻿#language: pt-br
+
+Funcionalidade: Listar pedidos enviados
+	Lista os pedidos que um usuário solicitou e estão pendentes de aprovação
+
+@mytag
+Cenario: Listar pedidos enviados
+	Quando o usuário <idUser> quiser ver a lista de pedidos enviados pendentes
+	Entao a lista de pedidos enviados deverá ser exibida <retorno>
+	Exemplos: 
+	| idUser                               | retorno                              |
+	| 2db41729-5cbc-4d81-ac9e-eca7f8edcb1f | d3d4b0f4-ee3b-4506-943c-62c88d50e874 |
+	#| d3d4b0f4-ee3b-4506-943c-62c88d50e874 | ???????? |
---
+++ MetWorkingMatch.Test/StepDefinitions/ListarPedidosEnviadosSteps.cs
@@ -0,0 +1,51 @@
+﻿using MetWorkingMatch.Application.Contracts;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+
+namespace MetWorkingMatch.Test.StepDefinitions
+{
+    [Binding]
+    public class ListarPedidosEnviadosSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly string _urlGetEnviados;
+
+        public ListarPedidosEnviadosSteps(ScenarioContext scenarioContext)
+        {
+            this._scenarioContext = scenarioContext;
+            this._urlGetEnviados = "https://localhost:5001/api/v1/PedidoMatch/enviados/";
+        }
+
+        [When(@"o usuário (.*) quiser ver a lista de pedidos enviados pendentes")]
+        public void QuandoOUsuarioQuiserVerAListaDePedidosEnviadosPendentes(Guid IdUser)
+        {
+            var request = new RestRequest();
+            request.Method = Method.GET;
+
+            request.Parameters.Clear();
+
+            var restClient = new RestClient(_urlGetEnviados + IdUser);
+
+            var response = restClient.Execute(request);
+
+            _scenarioContext["response"] = response;
+        }
+
+        [Then(@"a lista de pedidos enviados deverá ser exibida (.*)")]
+        public void EntaoAListaDePedidosPendentesDeveraSerExibida(Guid IdUserAmigo)
+        {
+            var response = (IRestResponse)_scenarioContext["response"];
+            var content = response.Content.ToString();
+
+            dynamic jsonResponse = JsonConvert.DeserializeObject<BaseResponse<PedidosMatchResponse>>(content);
+
+            BaseResponse<PedidosMatchResponse> finalResponse = new BaseResponse<PedidosMatchResponse>();
+            finalResponse = jsonResponse;
+
+            Assert.AreEqual(IdUserAmigo, finalResponse.Data.Pedidos[0].IdUser);
+        }
+    }
+}
