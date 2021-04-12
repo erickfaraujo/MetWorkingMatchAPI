c3263b4e4c12b5ab8fab1da2a01c1e224d9e55ae
Caio Ragazzi <ca.ragazzi@gmail.com>
2021-04-11 23:36:45 -0300

    ajuste delete

--- MetWorkingMatch.Infra/DependencyInjection.cs
+++ MetWorkingMatch.Infra/DependencyInjection.cs
@@ -11,7 +11,7 @@ namespace MetWorkingMatch.Infra
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseMySql(configuration.GetConnectionString("DefaultConnection"),
+                    options.UseMySql(configuration["METWORKING_CONNECTION"],
                                      b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
             services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
